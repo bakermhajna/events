6d680cc1a798482fdecae6b25042b710fdfd48a1
kha12 <mhajnabkr@gmail.com>
2025-01-02 23:32:41 +0200

    handiling the ExpiredJwtException

--- Events-Backend/src/main/java/com/events/events/config/JwtAuthenticationFilter.java
+++ Events-Backend/src/main/java/com/events/events/config/JwtAuthenticationFilter.java
@@ -4,15 +4,17 @@ package com.events.events.config;
 import com.events.events.models.Customer;
 import com.events.events.services.CustomerService;
 import com.events.events.services.JwtService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -20,6 +22,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -54,21 +59,32 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
 
         jwtToken = authHeader.substring(BEARER.length());
-        if (!jwtService.isTokenExpired(jwtToken)) {
-            id = jwtService.extractId(jwtToken);
-            if (id != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                Customer userDetails = this.customerService.findById(id);
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        userDetails,
-                        null,
-                        userDetails.getAuthorities()
-                );
-                authToken.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request)
-                );
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+        try{
+            if (!jwtService.isTokenExpired(jwtToken)) {
+                id = jwtService.extractId(jwtToken);
+                if (id != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                    Customer userDetails = this.customerService.findById(id);
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            userDetails,
+                            null,
+                            userDetails.getAuthorities()
+                    );
+                    authToken.setDetails(
+                            new WebAuthenticationDetailsSource().buildDetails(request)
+                    );
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
             }
+            filterChain.doFilter(request, response);
+        }catch (ExpiredJwtException ex){
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            Map<String, Object> errorResponse = new HashMap<>();
+            errorResponse.put("apiPath", request.getRequestURI());
+            errorResponse.put("errorCode", HttpServletResponse.SC_FORBIDDEN);
+            errorResponse.put("msg", ex.getMessage());
+            errorResponse.put("responseTime", LocalDateTime.now().toString());
+            response.getWriter().write(new ObjectMapper().writeValueAsString(errorResponse));
         }
-        filterChain.doFilter(request, response);
     }
 }
